469319d4d7bd8c2c576cd2e6798822e07e5fc94d
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2021-11-09 10:33:27 -0800

    Update simple-valk-trim.py

--- simple-valk-trim.py
+++ simple-valk-trim.py
@@ -6,10 +6,11 @@ pd.set_option('display.width', None)
 df = pd.read_csv(
     "C:/Users/Andrew Hu/Dropbox/PC/Downloads/Valkyrie IV data for cleaning.csv")
 df = df.sort_values(by='Measurement_Date-Time', ascending=True)
-#df['Measurement_Date-Time'] = pd.to_datetime(df['Measurement_Date-Time'], format='%d-%b-%y')
+df['Measurement_Date-Time'] = pd.to_datetime(df['Measurement_Date-Time'], format='%d-%b-%y')
 
 print(df.iloc[41,3] == df.iloc[44,3])
 #print(df.iloc[65:85,0:10])
+
 
 # int_DH = []
 # for i in range(1,len(df.index)):
@@ -20,6 +21,7 @@ print(df.iloc[41,3] == df.iloc[44,3])
 #             pass
 #     else:
 #         pass
+
 
 # int_DH = []
 # for i in range(1,len(df.index)):
@@ -33,6 +35,9 @@ print(df.iloc[41,3] == df.iloc[44,3])
 #         int_DH.append(df.iloc[i, :])
 #     else:
 #         pass
+#
+#
+#
 # final_DH = pd.concat(int_DH, axis=1)
 # final_DH = final_DH.transpose()
 newdf = df.drop_duplicates(subset=['Sample_ID'], keep='last')
