a123733859308b9702923e90dfa70936f17270b8
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-03-06 11:34:53 -0800

    Update simple-convectionh.py

--- simple-convectionh.py
+++ simple-convectionh.py
@@ -5,13 +5,13 @@ characteristiclength = 0.005
 
 #h = Nu*l / k
 #below will now be finding Nu
+#the convection solving for here is a flat plate
+#the convection is assumed to be forced, and laminar
 
 #Re = 100000
-Re = 50000
+Re = 50000 
 Pr = 0.71 #material constant for fluids
-
 Nu = (0.339*Re**(1/2)*Pr**(1/3))  /  ( 1+(0.0458/Pr)**(2/3) )**(1/4)
-
 
 h = (Nu*characteristiclength) / k
 
