4a5b50397b0c88cc2d0042d3c4136b2a3423fef1
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-03-17 00:28:22 -0700

    Update sample-T-peel-sequential.py

--- sample-T-peel-sequential.py
+++ sample-T-peel-sequential.py
@@ -5,8 +5,8 @@ import numpy as np
 
 
 
-xs = ["SUV 24", "SUV 100", "SUV100+HAST30","SUV100+HAST60"]
-xh = ["HAST 30", "HAST 100", "HAST100+SUV24","HAST100+SUV48"]
+xs = ["SUV 24", "SUV 100", "SUV100+HAST30","SUV100+HAST60","SUV100+HAST90"]
+xh = ["HAST 30", "HAST 100", "HAST100+SUV24","HAST100+SUV48","HAST100+SUV72"]
 #y = [1,1,1,1,2,10,10,12,8,10,13,13,13,13,13]
 fig = make_subplots(
     rows=2, cols=2,
@@ -15,28 +15,28 @@ fig = make_subplots(
 # agc sequential suv hast
 fig.add_trace(go.Scatter(
         x=xs,
-        y=[0, 0, 0, 0],
+        y=[0, 0, 0, 0, 0],
         name='AGC doubleside',
         marker_color="black"
     ),row=1,col=1)
 # etfe sequential suv hast
 fig.add_trace(go.Scatter(
         x=xs,
-        y=[33.9, 31.1, 31, 300],
+        y=[33.9, 31.1, 31, 35, 8.19],
         name='ETFE ctrl',
         marker_color="blue"
     ),row=1,col=2)
 # agc sequential hast -suv
 fig.add_trace(go.Scatter(
         x=xh,
-        y=[43, 42, 32.1, 16.4],
+        y=[43, 42, 32.1, 16.4, 14.212],
         name='AGC doubleside',
         marker_color="grey"
     ),row=2,col=1)
 # etfe sequential hast -suv
 fig.add_trace(go.Scatter(
         x=xh,
-        y=[38.62, 6.48, 0.88, 1.74],
+        y=[38.62, 6.48, 0.88, 1.74, 2.858],
         name='ETFE ctrl',
         marker_color="lightskyblue"
     ),row=2,col=2)
