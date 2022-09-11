9d48ce845ab1cde4de596726a0e74768cd8bb89c
homelaptop <andrewhu98@gmail.com>
2022-09-10 22:15:56 -0700

    ds200

--- PLOTTING/kaust-plot-DS200-0912.py
+++ PLOTTING/kaust-plot-DS200-0912.py
@@ -2,16 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#THIS IS MOST IMPORTANT VARIABLE, CHANGES THE WAFER BEING LOOKED AT
+start = 7 #14,21,28,35
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1MJoo_LZrhD4qlF2OCJEpl-r85inmDwYIydU-xvpqWhc/edit#gid=1050225185"
-
-
 url_1 = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
 df = pd.read_csv(url_1,)
 print(df)
 
 YREF = df.iloc[0:5,3]
-start = 7 #14,28,35
 YDAT = df.iloc[start:start+5,3]
 
 XREF = df.iloc[0:5,0]
@@ -20,7 +19,16 @@ XDAT = df.iloc[0:5,0]
 
 
 
+XMEAN = pd.DataFrame(np.zeros([5,1]))
+XMEAN = XMEAN + YDAT.mean()
+print(df.iloc[0,3]-YDAT.mean())
 plt.plot(XREF, YREF, 'k',label="Expected Print Diameter")
-plt.plot(XDAT, YDAT, 'k--',label="Real Print Diameter")
-#plt.scatter(df.iloc[0:8,1], df.iloc[0:8,3], alpha=0.5, label="Experimental Data")
+plt.plot(XDAT, YDAT, 'b',label="Real Print Diameter")
+plt.plot(XDAT, XMEAN, 'b--',label="Mean Print Diameter")
 plt.legend(loc="lower right")
+
+plt.title('Print Accuracy for {}'.format(df.iloc[start,2]))
+plt.xlabel('Print Hole Location')
+plt.ylabel('Print Diameter [mm]')
+plt.grid()
+plt.show()
