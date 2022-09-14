e2e4b63f20254b452fd94c0d8bc3cb2a24b1c862
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-09-13 21:25:16 -0700

    Update opencv-ds200-matlab.m

--- opencv-ds200-matlab.m
+++ opencv-ds200-matlab.m
@@ -2,4 +2,11 @@
 
 I = imread('IMG_002DS.jpg')
 imshow(I)
+h = imdistline
 %https://www.mathworks.com/help/images/measure-distance-between-pixels-in-image-viewer-app.html
+
+
+%Displayed at 25%
+%500 microns per 180 (scaled) pixels
+%500 microns per 720 pixels
+%0.69 microns per pixel
