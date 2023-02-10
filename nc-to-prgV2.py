d09a9fc000bc3b1d9b9245c852d593cedb7d31c4
homelaptop <andrewhu98@gmail.com>
2023-02-10 14:23:21 -0800

    dat new new

--- nc-to-prgV2.py
+++ nc-to-prgV2.py
@@ -1,21 +1,21 @@
 import numpy as np
 import pandas as pd
 
-# cell parameters needed here
-# busbarpitch = ??? currently set to 36.1
-# celldimensions = ?? 166 or 156.75
-# shinglewidth = ?? 156 or 133
-
-#testestest
-Power = 30 #not the module, the laser Power
-wsh = 25.50
+# all laser setting are set to ON by default, but movements lines need to be OFF
+# saved as a txt file, change end extension name to .prg
+# cell dimensions currently work for a M2 longi cell,
+# end cuts are 167mm, 10mm greater than cell width
+busbarpitch = 18.05 #currently set to 18.05 line 12 and 15
+Power = 30 #Setting for Sunic laser Power
+wsh = 26 #determines final shingle width needed
+busbartoedge = 8.5 #not implemented yet
 
 if (wsh >= 5) & (wsh < 18):
     wshcut = 18.05-wsh
 elif (wsh >= 18) & (wsh < 54):
     wshcut = 36.1-wsh
 else:
-    print("bruh why are you cutting shit that large/small")
+    print("width exceeds cell dimensions")
     exit()
 
 # DATAFRAME START initial three rows of laser cuts
@@ -23,7 +23,6 @@ sdf = pd.DataFrame(np.zeros([3,7])*np.nan)
 sdf.iloc[0,0:3] = 'Y-axis#', 0, 8.5
 sdf.iloc[1:3,0:3] = 'X-axis#', 167, 0
 # print(sdf)
-
 
 
 # DATAFRAME MIDDLE
@@ -43,9 +42,9 @@ for j in range(0,numloop):
         else:
             df.iloc[i,0:3] = "X-axis#", 167, 0
     iloop = iloop + 6
-# now the middle portion is finished
+# middle portion is finished
 # very last row y-axis 9.6 is extra, and doesn't need to be cut
-# code below goes through df, any NaN rows get dropped, along with the very last non-NaN y-axis value
+# code below goes through drops Nan, along with the very last non-NaN y-axis value
 is_NaN = df.isnull()
 for kk in range(0,len(df.index)):
     if is_NaN.iloc[kk,0] == True:
@@ -57,12 +56,12 @@ df.drop(labels=1, inplace=True) #second X-axis 0.0 row that isn't used
 df.drop(labels=ROWS-1, inplace=True) #additional NaN row that isn't used
 
 
-#DATAFRAME END
+#DTAFRAME END
 edf = pd.DataFrame(np.zeros([6,7])*np.nan)
 edf.iloc[0,0:3] = 'X-axis#', 3.625, 0
 edf.iloc[1,0:3] = 'Y-axis#', 0, -135
 edf.iloc[2,0:3] = 'Y-axis#', 0, 135
-edf.iloc[3,0:3] = 'X-axis#', 167-8.25, 0
+edf.iloc[3,0:3] = 'X-axis#', (167-8.5), 0
 edf.iloc[4,0:3] = 'Y-axis#', 0, -135
 edf.iloc[5,0:3] = 'Y-axis#', 0, 135
 #print(edf)
@@ -70,7 +69,7 @@ edf.iloc[5,0:3] = 'Y-axis#', 0, 135
 
 # compiles the start, middle, end into one single dataframe
 df = pd.concat([sdf, df, edf])
-# takes col 1 values and turns the 167, 167 into 167 -167
+# takes col 1 values and turns 167, 167 into 167 and -167
 for i in range(1,len(df.index)):
     if df.iloc[i-1,1] == 167:
         df.iloc[i,1] = -df.iloc[i,1]
@@ -86,6 +85,5 @@ df.iloc[:,2] = df.iloc[:,2].astype(str)+ '#'
 df.iloc[:,1] = df.iloc[:,1].astype(str)+ '#'
 print(df)
 df = df.to_string(header=False, index=False)
-
 #with open('C:/users/andre/Downloads/ncprgtest.txt', 'a') as f:
 #    f.write(df)
